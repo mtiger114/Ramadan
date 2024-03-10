257f9b475c87fdf123bdf821e0d74ea4c205261d
Mahmoud Fahmy <105823344+Ma7moudTiger@users.noreply.github.com>
2024-03-10 15:31:02 +0200

    Ramadan

---
+++ js.js
@@ -0,0 +1,9 @@
+function openFlaps() {
+  document.querySelector(".mi-flap1").classList.add("open");
+  document.querySelector(".mi-flap2").classList.add("open");
+}
+
+function closeFlaps() {
+  document.querySelector(".mi-flap1").classList.remove("open");
+  document.querySelector(".mi-flap2").classList.remove("open");
+}
